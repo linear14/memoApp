5f0d98bdfc602d6c593d9ef2cf0ed68d0a575545
linear14 <47495592+linear14@users.noreply.github.com>
2020-02-28 15:32:49 +0900

    Push notification at time set 1) Using Receiver 2) Data Transfer from fragment to receiver 3) Not update old alarm by new one, by using different requestCodes from memoVO
    -----
    
    시간 설정 푸시알람
    1) 리시버와 알람을 이용했음
    2) 프래그먼트에서 receiver로 데이터 이동(intent)
    3) 새로운 알람이 기존의 알람을 덮어버리지 못하도록 했음. 즉 여러 개의 알람을 설정하더라도 모두 작동함

--- app/src/main/java/com/dongldh/memoapp/MemoAlarmDialogFragment.kt
+++ app/src/main/java/com/dongldh/memoapp/MemoAlarmDialogFragment.kt
@@ -1,7 +1,9 @@
 package com.dongldh.memoapp
 
 import android.app.*
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -11,7 +13,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.DatePicker
 import android.widget.TimePicker
+import android.widget.Toast
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
@@ -31,11 +35,15 @@ class MemoAlarmDialogFragment : DialogFragment() {
 
         // 알림 시간 설정
         view.memo_alarm_time_setting.setOnClickListener() {
-            dismiss()
+            val bundle = Bundle()
+            bundle.putSerializable("memoVO", memoVO)
+
             val timeSettingDialogFragment = MemoAlarmTimeSettingDialogFragment()
+            timeSettingDialogFragment.arguments = bundle
             timeSettingDialogFragment.show(
                 activity?.supportFragmentManager as FragmentManager, "dialog_event"
             )
+            dismiss()
         }
 
         // 알림 표시줄에 설정 -> 바로 실행
@@ -78,34 +86,35 @@ class MemoAlarmDialogFragment : DialogFragment() {
 // 알림 시간 설정 했을 경우
 // https://www.tutorialkart.com/kotlin-android/android-datepicker-kotlin-example/ (DatePickerDialog)
 class MemoAlarmTimeSettingDialogFragment : DialogFragment() {
+    lateinit var memoVO: DataItemMemo
     var cal = Calendar.getInstance()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.activity_memo_alarm_timesetting_dialog_fragment, container, false)
+        memoVO = (arguments?.getSerializable("memoVO")) as DataItemMemo
         val text_alarm_date = view.text_alarm_date
         val text_alarm_time = view.text_alarm_time
         text_alarm_date.text = SimpleDateFormat("yyyy. MM. dd (E)", Locale.KOREA).format(cal.time)
         text_alarm_time.text = SimpleDateFormat("a hh:mm", Locale.KOREA).format(cal.time)
 
         // 날짜 선택 했을 때의 Listener
-        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
-            override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        val dateSetListener =
+            DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
                 cal.set(Calendar.YEAR, year)
                 cal.set(Calendar.MONTH, month)
                 cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
 
                 text_alarm_date.text = SimpleDateFormat("yyyy. MM. dd (E)", Locale.KOREA).format(cal.time)
             }
-        }
 
-        val timeSetListener = object : TimePickerDialog.OnTimeSetListener {
-            override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        val timeSetListener =
+            TimePickerDialog.OnTimeSetListener { view , hourOfDay, minute ->
                 cal.set(Calendar.HOUR_OF_DAY, hourOfDay)
                 cal.set(Calendar.MINUTE, minute)
+                cal.set(Calendar.SECOND, 0)
 
                 text_alarm_time.text = SimpleDateFormat("a hh:mm", Locale.KOREA).format(cal.time)
             }
-        }
 
 
         // 날짜를 선택했을 때 DatePicker 보여줌
@@ -124,11 +133,59 @@ class MemoAlarmTimeSettingDialogFragment : DialogFragment() {
         }
 
         // 확인버튼. 지정된 시간에 Notification 뜨도록 설정함
+        // https://kwongyo.tistory.com/5
         view.button_ok_setting.setOnClickListener() {
+            dismiss()
+            // 이것도 마찬가지로 그냥 getSystemService를 사용하면 안됨 (getSystemService는 Context내의 메서드라서 context를 불러줘야됨(Fragment는 Context를 상속 안받음). 따라서 Fragment 내에서의 사용은 아래와 같음)
+            val am = context?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            val intent = Intent(this.context, BroadCastAlarm::class.java)
+            intent.putExtra("num", memoVO.num)
+            intent.putExtra("title", memoVO.title)
+            intent.putExtra("content", memoVO.content)
+            val sender = PendingIntent.getBroadcast(this.context, memoVO.num, intent, 0)
 
+            val selectedTime = cal.timeInMillis
+
+            // log. 입력 된 시간 (문제 없음)
+            // Toast.makeText(context, SimpleDateFormat("hh : mm : ss").format(selectedTime), Toast.LENGTH_SHORT).show()
+
+            am.set(AlarmManager.RTC_WAKEUP, selectedTime, sender)
         }
 
         return view
+    }
+}
+
+// 문제점 두 가지
+// 1. 알림 설정을 두개의 항목에다가 각각 설정하면, update가 되어서, 마지막에 설정한 알림이 작동한다. --> pendingIntent.getActivity의 두번쨰 param를 memoVO.num으로 제각기 다르게 설정으로 해결가능성)
+// 2. 딱 0초에 실행되는 것이 아니라.. 제 각각이다. --> setWhen이 의심스러움.. 이따가 확인
+class BroadCastAlarm: BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        // log
+        // Toast.makeText(context, SimpleDateFormat("hh : mm : ss").format(System.currentTimeMillis()), Toast.LENGTH_LONG).show()
+
+        // 방아올 때, data class 자체를 객체로 받아오는 것이 아니라, 각각의 필요한 데이터를 intent에서 넘겨주어야 함.
+        // https://stackoverflow.com/questions/39209579/how-to-pass-custom-serializable-object-to-broadcastreceiver-via-pendingintent 참고
+        val num = intent.getIntExtra("num", 0)
+        val title = intent.getStringExtra("title")
+        val content = intent.getStringExtra("content")
+
+        val notificationManager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val pendingIntent = PendingIntent.getActivity(context, num, Intent(context, MainActivity::class.java), PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val builder = NotificationCompat.Builder(context, "default")
+        builder.setSmallIcon(R.mipmap.ic_launcher)
+        builder.setContentTitle(title)
+        builder.setContentText(content)
+        builder.color = Color.BLUE
+        builder.setContentIntent(pendingIntent)
+        builder.setWhen(System.currentTimeMillis())
+        builder.setAutoCancel(true)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(NotificationChannel("default", "기본 채널", NotificationManager.IMPORTANCE_DEFAULT))
+        }
+        notificationManager.notify(2, builder.build())
     }
 
 }
